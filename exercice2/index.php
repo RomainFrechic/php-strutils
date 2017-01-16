29b0fb1bc2482ef92d965cc3a6d2701d3cb04dd5
Romain <RomainFrechic@outlook.fr>
2017-01-16 15:30:27 +0100

    poo php formulaire

--- exercice2/index.php
+++ exercice2/index.php
@@ -2,37 +2,45 @@
 
 	class User {
 
-		private $useVariable = 'name';
+		private $useVariable = name;
 
-	public function answerUser($useVariable){
+		public function answerUser($useVariable){
 			foreach ($_POST as $value) {
 				echo $value;
 			}
 		}
-	}
-
-	 $User = new User();
-	 //echo $User -> answerUser();
 
 
-	?>
+		public function cache(){
+		
+				$file = $_FILES['nomfichier'];
+				$extention = substr($file['name'],-3);
+				
+				if($extention === 'pdf'){
+					echo 'Bravo ! le fichier '.$extention.' à été télécharger';
+				}else{
+					echo 'ERREUR EXTENSION FICHIER IS NOT PDF';
+				}
+			}
+		}
 
-	<!DOCTYPE html>
-	<html lang="en">
-	<head>
-		<meta charset="UTF-8">
-		<title>PHP POO exercice 2</title>
-	</head>
-	<body>
-		<form action="index.php" method="POST">
-			<div>		
-				<label for="name">Nom</label>
-				<input type="text" value=" Charlie " name="name" id="name">
-				<input type="text" value=" Chaplin " name="firstname" id="name">
-			</div>
-			<button>Envoyer</button>
+		$User = new User();
+		//echo $User -> answerUser();
+		//echo $User -> cache();
 
-			<h1><?php echo $User -> answerUser(); ?></h1>
-		</form>
-	</body>
-	</html>
+		?>
+
+		<form enctype="multipart/form-data" action="index.php" method="POST" class="formulaire">
+
+			<label for="name">Nom</label>
+			<select name="Genre">
+				<option value="Mr ">Mr</option>
+				<option value="Mme ">Mme</option></select>
+				<input type="text" value="John " name="name" id="name">	
+				<input type="text" value="Lennon " name="prenom" id="prenom"></br>
+				<input type="file" name="nomfichier" /></br>
+				<button type="submit">Envoyer</button>
+			</form>
+        <h1><?php echo $User -> answerUser(); ?></h1>
+        <h1><?php echo $User -> cache(); ?></h1>
+
