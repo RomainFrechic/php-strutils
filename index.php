1b876d871b661eafca94ce5a62eb69260abe7689
Romain <RomainFrechic@outlook.fr>
2017-01-16 11:50:40 +0100

    exercice uglify

--- index.php
+++ index.php
@@ -2,49 +2,56 @@
 
 class StrUtils{
 
-	   private $str ='PHP LA PROGRAMATION OBJET !';
+	private $str ='PHP LA PROGRAMATION OBJET !';
 
 
-	    public function __construct($str){
-	   	$this->str=$str;
-	   }
+	public function __construct($str){
+		$this->str=$str;
+	}
 
-	   public function Bold($str){
-	   	return '<strong>'.$this->str.'</strong>';
-	   }
+	public function Bold($str){
+		return '<h3>BOLD :</h3><strong>'.$this->str.'</strong>';
+	}
 
-	    public function Italic($str){
-	   	return '<i>'.$this->str.'</i>';
-	   }
+	public function Italic($str){
+		return '<h3>ITALIC :</h3><i>'.$this->str.'</i>';
+	}
 
-	    public function Underline($str){
-	   	return '<u>'.$this->str.'</u>';
-	   }
-	   public function Capitalize($str){
-	   	return strtoupper($this->str);
-	   }
+	public function Underline($str){
+		return '<h3>UNDERLINE :</h3><u>'.$this->str.'</u>';
+	}
 
+	public function Capitalize($str){
+		return strtoupper('<h3>CAPITALIZE :</h3>'.$this->str);
+	}
+
+	public function Uglify($str){
+		return '<h3>UGLIFY :</h3><strong><i><u>'.$this->str.'</u></i></strong><i>';
+	}
 
 
 }
 
-  	$StrUtils = new StrUtils('PHP IS AWESOME IN BOLD!</br>');
-     print_r($StrUtils->Bold($str));  
+$StrUtils = new StrUtils('PHP IS AWESOME IN BOLD!</br>');
+print_r($StrUtils->Bold($str));  
 
 
-  	$StrUtils = new StrUtils('PHP IS AWESOME IN ITALIC!</br>');
-     print_r($StrUtils->Italic($str));
+$StrUtils = new StrUtils('PHP IS AWESOME IN ITALIC!</br>');
+print_r($StrUtils->Italic($str));
 
-     $StrUtils = new StrUtils('PHP IS AWESOME IN UNDERLINE!</br>');
-     print_r($StrUtils->Underline($str));
+$StrUtils = new StrUtils('PHP IS AWESOME IN UNDERLINE!</br>');
+print_r($StrUtils->Underline($str));
 
-      $StrUtils = new StrUtils('php is awesome in Capitalize!</br>');
-     print_r($StrUtils->Capitalize($str));        
+$StrUtils = new StrUtils('php is awesome in Capitalize!</br>');
+print_r($StrUtils->Capitalize($str));
 
-
-
-
-
+$StrUtils = new StrUtils('php is awesome in Capitalize!</br>');
+print_r($StrUtils->Uglify($str));             
 
 
- ?>
+
+
+
+
+
+?>
